6a11703fe63818a96f5be4acdc0b01d9c77c8799
kimuit117 <kemoikiuita@gmail.com>
2022-08-22 14:45:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply=num1*num2;
+
+let random;
+random=Math.ceil(Math.random()*10+1);
+let mod;
+let num3=24;
+let num4=5;
+mod=num3%num4;
+// console.log(mod);
+let max;
+max=Math.max(10,20,3,0,1,16,17,18,19,11);
+// return max;
